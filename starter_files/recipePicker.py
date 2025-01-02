26a764c497a977a22e0b8595ca3966d5c82ecd4d
Personal_PC <karthik.tadakapalli@gmail.com>
2025-01-02 01:29:45 -0600

    Added Frame

--- starter_files/recipePicker.py
+++ starter_files/recipePicker.py
@@ -1,7 +1,16 @@
 import tkinter as tk
+from PIL import ImageTk
 
 # initiallize app
 root = tk.Tk()
+root.title("Recipe Picker")
+root.eval("tk::PlaceWindow . center")
+
+frame1 = tk.Frame(root, width =500, height=600, bg="#3d6466")
+frame1.grid(row=0, column = 0)
+
+#Create a frame Widget
+ImageTk.PhotoImage
 
 # run app
 root.mainloop()
