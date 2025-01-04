937151e5767dc08385c09626224ec2b8807b406e
Personal_PC <karthik.tadakapalli@gmail.com>
2025-01-04 02:55:50 -0600

    Updated code to add frames and working app

--- starter_files/recipePicker.py
+++ starter_files/recipePicker.py
@@ -5,6 +5,9 @@ from numpy import random
 
 bg_colour = "#3d6466"
 
+def clear_widgets(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
 
 def fetch_db():
     connection = sqlite3.connect("data/recipes.db")
@@ -12,17 +15,37 @@ def fetch_db():
     cursor.execute("SELECT * FROM sqlite_schema WHERE type ='table';")
     all_tables = cursor.fetchall()
 
-    idx = random.randint(0, len(all_tables)-1)
+    idx = random.randint(0, len(all_tables) - 1)
 
-    #fetch ingredients
+    # fetch ingredients
     table_name = all_tables[idx][1]
     cursor.execute("SELECT * FROM " + table_name + ";")
-    ingredients = cursor.fetchall()
+    table_records = cursor.fetchall()
 
     print(all_tables[idx])
     connection.close()
+    return table_name, table_records
+
+
+def pre_process(table_name, table_records):
+    title = table_name[:-6]
+    title = "".join([char if char.islower() else " " + char for char in title])
+    print(title)
+
+    ingredients = []
+    # ingredients
+    for i in table_records:
+        name = i[1]
+        qty = i[2]
+        unit = i[3]
+
+        ingredients.append(qty + " " + unit + " of " + name)
+    return title, ingredients
+
 
 def load_frame1():
+    clear_widgets(frame2)
+    frame1.tkraise()
     frame1.propagate(False)
     # Create a frame Widget
     # logo Widget
@@ -53,10 +76,47 @@ def load_frame1():
 
 
 def load_frame2():
-    fetch_db()
+    clear_widgets(frame1)
+    frame2.tkraise()
+    table_name, table_records = fetch_db()
+    title, ingredients = pre_process(table_name, table_records)
+
+    # logo Widget
+    logo_img = ImageTk.PhotoImage(file="assets/RRecipe_logo_bottom.png")
+    logo_widget = tk.Label(frame2, image=logo_img, bg=bg_colour)
+    logo_widget.image = logo_img
+    logo_widget.pack(pady=20)
+
+    tk.Label(
+        frame2,
+        text=title,
+        bg=bg_colour,
+        fg="white",
+        font=("TkHeadingFont", 20)
+    ).pack(pady=25)
+
+    for i in ingredients:
+        tk.Label(
+            frame2,
+            text=i,
+            bg="#28393a",
+            fg="white",
+            font=("TkHeadingFont", 12)
+        ).pack(fill="both")
+
+    tk.Button(
+        frame2,
+        text="Back",
+        font=("TkHeadingFont", 18),
+        bg="#28393a",
+        fg="white",
+        cursor="hand2",
+        activebackground="#badee2",
+        activeforeground="black",
+        command=lambda: load_frame1()).pack()
 
 
-#initiallize app
+# initiallize app
 root = tk.Tk()
 root.title("Recipe Picker")
 root.eval("tk::PlaceWindow . center")
@@ -64,9 +124,8 @@ root.eval("tk::PlaceWindow . center")
 frame1 = tk.Frame(root, width=500, height=600, bg=bg_colour)
 frame2 = tk.Frame(root, bg=bg_colour)
 
-
 for frame in (frame1, frame2):
-    frame.grid(row=0, column=0)
+    frame.grid(row=0, column=0, sticky="nesw")
 
 load_frame1()
 
